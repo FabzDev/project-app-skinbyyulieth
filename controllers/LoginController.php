c6f80e5ef938a682c4db7b63c9d5025628e87c95
Fabio <fabioescobarardila@hotmail.com>
2024-10-24 23:51:03 -0500

    Vista creada: Olvide mi password

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -5,24 +5,30 @@ namespace Controllers;
 use MVC\Router;
 
 class LoginController {
-    public static function login(Router $instRouter){
-        $instRouter->render('auth/login');
+    public static function login(Router $inst1Router){
+        $inst1Router->render('auth/login', [
+
+        ]);
     }
 
     public static function logout(){
         echo "Desde Logout";
     }
 
-    public static function olvide(){
-        echo "Desde Olvide";
+    public static function olvide(Router $inst2Router){
+        $inst2Router->render('auth/olvide-password', [
+
+        ]);
     }
 
     public static function recuperar(){
         echo "Desde Recuperar";
     }
 
-    public static function crear(Router $inst2Router){
-        $inst2Router->render('auth/crear-cuenta');
+    public static function crear(Router $inst3Router){
+        $inst3Router->render('auth/crear-cuenta', [
+            
+        ]);
     }
 
 }
---
+++ views/auth/olvide-password.php
@@ -0,0 +1,20 @@
+<h1 class="titulo-pagina">Olvide Password</h1>
+<p class="descripcion-pagina">Ingresa tu E-mail para restablecer tu Password</p>
+<form class="formulario" method="POST" action="submit">
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input 
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Ingresa tu E-mail"
+        />
+
+    </div>
+    <input class="boton" type="submit" value="Restablecer Password">
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Iniciar Sesión</a>
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear cuenta</a>
+</div>
