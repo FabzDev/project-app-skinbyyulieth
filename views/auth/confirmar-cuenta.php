e6b867f25a879478cfeb6dfcffd6d584c75946a5
Fabio <fabioescobarardila@hotmail.com>
2024-10-27 23:14:40 -0500

    Completado envio de emails para validacion de correo por medio de tokens

--- views/auth/confirmar-cuenta.php
+++ views/auth/confirmar-cuenta.php
@@ -1,5 +1,9 @@
 <h1></h1><h1 class="nombre-pagina">Confirmar Cuenta</h1>
-<p class="descripcion-pagina">Confirmación en proceso...</p>
+<!-- <p class="descripcion-pagina">Confirmación en proceso...</p> -->
 <?php
     include_once __DIR__ . "/../templates/alertas.php"
 ?>
+
+<div class="acciones confirmarCuenta">
+    <a href="/">Iniciar Sesión con mi cuenta</a>
+</div>
--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,2 +1,6 @@
 <h1 class="nombre-pagina">Crear Cuenta</h1>
 <p class="descripcion-pagina">Hemos enviado a tu email las instrucciones para activar tu cuenta</p>
+
+<div class="acciones confirmarCuenta">
+    <a href="/">Iniciar Sesión con mi cuenta</a>
+</div>
