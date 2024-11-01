2f0e0ce863139d16d614612dd0277b39ebe71a5c
Fabio <fabioescobarardila@hotmail.com>
2024-11-01 17:47:38 -0500

    Creando modulo citas 10%

--- public/index.php
+++ public/index.php
@@ -27,5 +27,9 @@ $router->post('/crear-cuenta',[LoginController::class, 'crear']);
 $router->get('/confirmar-cuenta',[LoginController::class, 'confirmar']);
 $router->get('/mensaje',[LoginController::class, 'mensaje']);
 
+//Citas
+$router->get('/citas',[LoginController::class, 'index']);
+
+
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
