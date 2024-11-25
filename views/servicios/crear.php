2b8b403843ed894a60771ae3cb8d546965e5c655
Fabio <fabioescobarardila@hotmail.com>
2024-11-25 07:21:48 -0500

    Formulario servicio creado

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -5,4 +5,7 @@
 <h1 class="nombre-pagina">Crear nuevo Servicio</h1>
 <p class="descripcion-pagina">Ingresa la información del servicio</p>
 
-
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . "/formulario.php" ?>
+    <input type="submit" class="boton" value="Guardar Servicio">
+</form>
