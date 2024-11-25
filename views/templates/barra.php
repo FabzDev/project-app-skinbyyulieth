476386df50fc887eefaea2133c59d807d531d59e
Fabio <fabioescobarardila@hotmail.com>
2024-11-24 21:55:31 -0500

    Barra lateral, html y css completo

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -2,3 +2,16 @@
     <p>Hola, <?php echo $nombre ?? '';?></p>
     <a class="boton" href="/logout">Cerrar Sesión</a>
 </div>
+
+<!-- PHP con html -->
+ <?php
+    if($_SESSION['admin']){
+?>  
+    <div class="barra-servicios">
+        <a href="/admin" class="boton">Ver Citas</a>
+        <a href="/servicios" class="boton">Ver Servicios</a>
+        <a href="/servicios/nuevo" class="boton">Nuevo Servicio</a>
+    </div>      
+<?php
+    }
+?>
