103a427406aaa8c48557b0b613440c2648494db9
Fabio <fabioescobarardila@hotmail.com>
2024-11-24 23:07:11 -0500

    Implementando servicios views index, crear, modificar

---
+++ views/servicios/crear.php
@@ -0,0 +1,8 @@
+<?php
+    include_once __DIR__ . "/../templates/barra.php"
+?>
+
+<h1 class="nombre-pagina">Crear nuevo Servicio</h1>
+<p class="descripcion-pagina">Ingresa la información del servicio</p>
+
+
---
+++ views/servicios/index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once __DIR__ . "/../templates/barra.php"
+?>
+
+<h1 class="nombre-pagina">Administrador de Servicios</h1>
+<p class="descripcion-pagina">Información de servicios</p>
+
+
