5726340846a09659c7c7799b1ba96124e93a4cd3
Fabztech <fabztech@outlook.com>
2025-02-23 17:01:02 -0500

    Testing quotes modification in query

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -18,19 +18,19 @@ class AdminController
             header('Location: /404');
         };
 
-        $consulta = 'SELECT citas.id, citas.hora, CONCAT(usuarios.nombre," ",usuarios.apellido) as cliente, usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio';
-        $consulta .= ' FROM citas';
-        $consulta .= ' LEFT OUTER JOIN usuarios';
-        $consulta .= ' ON citas.usuarioId=usuarios.id';
-        $consulta .= ' LEFT OUTER JOIN citas_servicios';
-        $consulta .= ' ON citas.id=citas_servicios.citasId';
-        $consulta .= ' LEFT OUTER JOIN servicios';
+        $consulta = "SELECT citas.id, citas.hora, CONCAT(usuarios.nombre,' ',usuarios.apellido) as cliente, usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio";
+        $consulta .= " FROM citas";
+        $consulta .= " LEFT OUTER JOIN usuarios";
+        $consulta .= " ON citas.usuarioId=usuarios.id";
+        $consulta .= " LEFT OUTER JOIN citas_servicios";
+        $consulta .= " ON citas.id=citas_servicios.citasId";
+        $consulta .= " LEFT OUTER JOIN servicios";
         
-        $consulta .= ' ON citas_servicios.serviciosId=servicios.id;';
-        // $consulta .= ' ON citas_servicios.serviciosId=servicios.id';
-        // $consulta .= ' WHERE fecha="'.$fechaSel.'";';
+        // $consulta .= ' ON citas_servicios.serviciosId=servicios.id;';
+        $consulta .= " ON citas_servicios.serviciosId=servicios.id";
+        $consulta .= " WHERE fecha='".$fechaSel."';";
 
-        // debuguear($consulta);
+        debuguear($consulta);
 
         $resultado = AdminCita::SQL($consulta);
         
